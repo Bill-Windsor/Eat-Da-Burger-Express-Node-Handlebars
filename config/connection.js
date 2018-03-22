40e18fce53cdd423e528b853f5cab02321f1bb16
Bill-Windsor <bwindsor888@icloud.com>
2018-03-22 09:26:30 -0700

    commit for heroku/jawsDB deployment

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,13 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  }
+else { 
+  connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
   user: "root",
@@ -17,6 +23,5 @@ connection.connect(function(err) {
   }
   console.log("Connected as id: " + connection.threadId);
 });
-
 // Export connection for our ORM to use.
 module.exports = connection;
