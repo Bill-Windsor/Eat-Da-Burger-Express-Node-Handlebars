0a48b35ac4b0d7954534f666925c2ef4178a325d
Bill-Windsor <bwindsor888@icloud.com>
2018-03-29 09:40:21 -0700

    push with fix for Heroku favicon issue

--- db/y2grvx4scz253cls.sql
+++ db/y2grvx4scz253cls.sql
@@ -1,16 +1,14 @@
-DROP DATABASE IF EXISTS burgers_db;
-CREATE DATABASE burgers_db;
-USE burgers_db;
+DROP DATABASE IF EXISTS y2grvx4scz253cls;
+CREATE DATABASE y2grvx4scz253cls;
+USE y2grvx4scz253cls;
 
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
+CREATE TABLE burgers (
+	id INT AUTO_INCREMENT NOT NULL,
+	burger_name VARCHAR(255),
 	devoured BOOLEAN NOT NULL DEFAULT false,
-    date TIMESTAMP,
+    createdAt TIMESTAMP NOT NULL,
 	PRIMARY KEY (id)
 );
-
 INSERT INTO burgers(burger_name) VALUES ('Mushroom Swiss Burger');
 INSERT INTO burgers(burger_name) VALUES ('Bacon Bleu Burger');
 INSERT INTO burgers(burger_name) VALUES ('Tofurkey Burger');
